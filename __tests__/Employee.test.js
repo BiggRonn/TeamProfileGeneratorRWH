3c780e75715452eace9d71156c5265ef899b0c90
Wes Howard <BigRonald42@hotmail.com>
2021-03-19 20:52:20 -0700

    Employee class built, tests complete

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,3 +5,13 @@ test("Can return Employee name", () => {
     const employee = new Employee("Dan", 23352, "big@maily.com");
     expect(employee.getName()).toBe("Dan");
 })
+
+test("Can return Employee ID", () => {
+    const employee = new Employee("Dan", 23352, "big@maily.com");
+    expect(employee.getId()).toBe(23352);
+})
+
+test("Can return Employee Email", () => {
+    const employee = new Employee("Dan", 23352, "big@maily.com");
+    expect(employee.getEmail()).toBe("big@maily.com");
+})
